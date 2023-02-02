9a7f0cc6c8eeea6144e286ef9d59e50957bf917f
Ollie Backhouse <olbackhouse@gmail.com>
2023-02-02 17:20:25 +0000

    Adds wick_uscf type dict

--- pygnme/__init__.py
+++ pygnme/__init__.py
@@ -8,15 +8,23 @@ from libpygnme import wick, utils
 
 
 wick.wick_orbitals = {
-        (float, float): wick.wick_orbitals_double_double,
-        (complex, float): wick.wick_orbitals_complex_double,
-        (complex, complex): wick.wick_orbitals_complex_complex,
+    (float, float): wick.wick_orbitals_double_double,
+    (complex, float): wick.wick_orbitals_complex_double,
+    (complex, complex): wick.wick_orbitals_complex_complex,
 }
 
 
 wick.wick_rscf = {
-        (float, float, float): wick.wick_rscf_double_double_double,
-        (complex, float, float): wick.wick_rscf_complex_double_double,
-        (complex, complex, float): wick.wick_rscf_complex_complex_double,
-        (complex, complex, complex): wick.wick_rscf_complex_complex_complex,
+    (float, float, float): wick.wick_rscf_double_double_double,
+    (complex, float, float): wick.wick_rscf_complex_double_double,
+    (complex, complex, float): wick.wick_rscf_complex_complex_double,
+    (complex, complex, complex): wick.wick_rscf_complex_complex_complex,
+}
+
+
+wick.wick_uscf = {
+    (float, float, float): wick.wick_uscf_double_double_double,
+    (complex, float, float): wick.wick_uscf_complex_double_double,
+    (complex, complex, float): wick.wick_uscf_complex_complex_double,
+    (complex, complex, complex): wick.wick_uscf_complex_complex_complex,
 }
